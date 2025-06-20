32bd47c5b52003e4730db8f2ae4f92995120a5af
jaerrib <jaerrib@tutanota.com>
2025-06-20 05:25:07 -0400

    Rotate snake head to match travel direction

--- scenes/snake/snake.gd
+++ scenes/snake/snake.gd
@@ -5,10 +5,13 @@ var speed = 50
 
 @onready var sprite_2d: Sprite2D = $Sprite2D
 
+
 func _physics_process(delta: float) -> void:
 	get_input()
 	velocity = move_direction * speed
 	move_and_slide()
+	rotate_sprite()
+
 
 func get_input():
 	if Input.is_action_just_pressed("down") and move_direction != Vector2.UP:
@@ -19,3 +22,15 @@ func get_input():
 		move_direction = Vector2.LEFT
 	elif Input.is_action_just_pressed("right") and move_direction != Vector2.LEFT:
 		move_direction = Vector2.RIGHT
+
+
+func rotate_sprite():
+	match move_direction:
+		Vector2.UP:
+			sprite_2d.rotation_degrees = 180
+		Vector2.DOWN:
+			sprite_2d.rotation_degrees = 0
+		Vector2.LEFT:
+			sprite_2d.rotation_degrees = 90
+		Vector2.RIGHT:
+			sprite_2d.rotation_degrees = -90
