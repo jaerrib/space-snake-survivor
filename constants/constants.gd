5b9f54f91f3d1164e30b26125b9acad902ac38a7
jaerrib <jaerrib@tutanota.com>
2025-06-23 05:56:04 -0400

    Add basic player projectile

--- constants/constants.gd
+++ constants/constants.gd
@@ -2,4 +2,8 @@ class_name Constants
 
 enum EnemyType { ASTEROID_SMALL, ASTEROID_MEDIUM, ASTEROID_LARGE }
 
+enum ProjectileType { SNAKE_ATTACK_1 }
+
+enum PlayerWeapons { ATTACK_1 }
+
 enum ObjectType { XP }
