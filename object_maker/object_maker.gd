e8d125a2abd09ab28be25ffd466939ef5362f6e7
jaerrib <jaerrib@tutanota.com>
2025-06-23 05:54:08 -0400

    Set up base projectile

--- object_maker/object_maker.gd
+++ object_maker/object_maker.gd
@@ -12,9 +12,15 @@ const OBJECT_SCENES: Dictionary = {
 	Constants.ObjectType.XP: preload("res://scenes/xp/xp.tscn")
 }
 
+const PROJECTILE_SCENE: Dictionary = {
+	Constants.ProjectileType.SNAKE_ATTACK_1: preload("res://scenes/snake_projectile_1/snake_projectile_1.tscn")
+}
+
+
 func _ready() -> void:
 	SignalManager.on_create_enemy.connect(on_create_enemy)
 	SignalManager.on_create_object.connect(on_create_object)
+	SignalManager.on_create_projectile.connect(on_create_projectile)
 	
 	
 func on_create_enemy(position: Vector2, enemy_type: Constants.EnemyType):
@@ -29,3 +35,17 @@ func on_create_object(position: Vector2, object_type: Constants.ObjectType) -> v
 	var new_object = OBJECT_SCENES[object_type].instantiate()
 	new_object.position = position
 	call_deferred("add_child", new_object)
+
+
+func on_create_projectile(
+	start_pos: Vector2,
+	direction: Vector2,
+	speed: float,
+	flip_sprite: bool,
+	projectile_type: Constants.ProjectileType ) -> void:
+	if !PROJECTILE_SCENE.has(projectile_type):
+		return
+	var scene = PROJECTILE_SCENE[projectile_type].instantiate()
+	scene.position = start_pos
+	scene.setup(direction, speed, flip_sprite)
+	call_deferred(ADD_OBJECT, scene, start_pos)
--- singletons/signal_manager.gd
+++ singletons/signal_manager.gd
@@ -5,5 +5,12 @@ signal on_create_object(
 	position: Vector2,
 	object_type: Constants.ObjectType,
 )
+signal on_create_projectile(
+	position: Vector2, 
+	direction: Vector2, 
+	speed: float,
+	flip_sprite: bool,
+	projectile_type: Constants.ProjectileType,
+	)
 signal on_rotate_snake(direction: Vector2)
 signal on_xp_touched(val: int)
