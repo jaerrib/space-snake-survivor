562f7aa81926fd7bd66837ba7b87431ad330dce2
jaerrib <jaerrib@tutanota.com>
2025-06-27 12:58:32 -0400

    Add basic health bar to hud

--- scenes/health_bar/health_bar.gd
+++ scenes/health_bar/health_bar.gd
@@ -4,14 +4,36 @@ const COLOR_DANGER: Color = Color("#cc0000")
 const COLOR_MIDDLE: Color = Color("#ff9900")
 const COLOR_GOOD: Color = Color("#33cc33")
 
-@export var level_low: float
-@export var level_med: float
-@export var max_health: float
 @export var start_health: float
+@export var level_med: float = start_health * .66
+@export var level_low: float= start_health * .33
+
+var player_ref: Snake
+
 
 func _ready() -> void:
 	var player: Snake = get_tree().get_first_node_in_group("player")
 	start_health = player.get_health()
-	max_health = player.get_max_health()
-	level_med = start_health * .66
-	level_med = start_health * .33
+	max_value = player.get_max_health()
+	max_value
+	value = start_health
+	player_ref = player
+	SignalManager.on_level_up.connect(on_level_up)
+
+
+func _process(delta: float) -> void:
+	value = player_ref.get_health()
+	set_color()
+
+
+func set_color() -> void:
+	if value < level_low:
+		tint_progress = COLOR_DANGER
+	elif value < level_med:
+		tint_progress = COLOR_MIDDLE
+	else:
+		tint_progress = COLOR_GOOD
+
+
+func on_level_up() -> void:
+	max_value = player_ref.get_max_health()
