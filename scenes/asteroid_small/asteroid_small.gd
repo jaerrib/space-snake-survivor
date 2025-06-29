67ed57ff35ee8d585c645fc7e8ae33fb89d5718a
jaerrib <jaerrib@tutanota.com>
2025-06-29 09:10:57 -0400

    Create small asteroid variant

--- scenes/asteroid_small/asteroid_small.gd
+++ scenes/asteroid_small/asteroid_small.gd
@@ -1,8 +1,9 @@
 class_name AsteroidSmall extends EnemyBase
 
+@export var sprite_frame: int
+
 @onready var sprite_2d: Sprite2D = $Sprite2D
 
 
 func _ready() -> void:
-	var frame_number: int = randi_range(0, 1)
-	sprite_2d.frame = frame_number
+	sprite_2d.frame = sprite_frame
