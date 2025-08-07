830d7462aca88b0ba21bf4c061a7567c1bfcd068
jaerrib <jaerrib@tutanota.com>
2025-08-07 05:46:38 -0400

    Add class name

--- scenes/station/station.gd
+++ scenes/station/station.gd
@@ -1,6 +1,7 @@
-extends CharacterBody2D
+class_name Station extends CharacterBody2D
 
 @export var damage_percentage: float = .05
+
 
 func get_damage() -> int:
 	var player_ref: Snake =  get_tree().get_first_node_in_group("player")
