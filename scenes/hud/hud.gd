7f9b05dc53035fd70df70a4c55949060dcf84474
jaerrib <jaerrib@tutanota.com>
2025-08-26 12:27:33 -0400

    Move debug info to HUD, add shortcut to toggle

--- scenes/hud/hud.gd
+++ scenes/hud/hud.gd
@@ -6,12 +6,16 @@ const LEVEL_LENGTH: int = 1800 #in seconds
 @onready var time_label: Label = $MC/MC2/HB/TimeLabel
 @onready var xp_level_label: Label = $MC/MC2/HB/XPLevelLabel
 @onready var scan_lines: ColorRect = $"../ScanLines"
-@onready var fps_label: Label = $MarginContainer/FPSLabel
-
+@onready var debug_info: MarginContainer = $DebugInfo
+@onready var fps_label: Label = $DebugInfo/VBoxContainer/FPSLabel
+@onready var enemy_count_label: Label = $DebugInfo/VBoxContainer/EnemyCountLabel
+@onready var object_count_label: Label = $DebugInfo/VBoxContainer/ObjectCountLabel
+@onready var projectile_count_label: Label = $DebugInfo/VBoxContainer/ProjectileCountLabel
 
 
 var player_ref: Snake
 var time_elapsed: int
+var debug: bool = false
 
 
 func _ready() -> void:
@@ -20,12 +24,17 @@ func _ready() -> void:
 	xp_level_label.text = str(player_ref.get_level())
 	SignalManager.on_level_up.connect(on_level_up)
 	SignalManager.on_player_died.connect(on_player_died)
+	debug_info.visible = debug
 
 
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("toggle-scanlines"):
 		scan_lines.visible = !scan_lines.visible
-	fps_label.text = "FPS %d" % Engine.get_frames_per_second()
+	if Input.is_action_just_pressed("toggle-debug"):
+		debug = !debug
+		debug_info.visible = !debug_info.visible
+	if debug:
+		update_debug_labels()
 
 
 func update_timer_label() -> void:
@@ -47,3 +56,11 @@ func on_level_up() -> void:
 
 func on_player_died() -> void:
 	print("DEAD")
+
+
+func update_debug_labels() -> void:
+	var object_maker: ObjectMaker = get_tree().root.get_node("Main/Level/ObjectMaker")
+	fps_label.text = "FPS %d" % Engine.get_frames_per_second()
+	enemy_count_label.text = "Enemies %d" % object_maker.get_enemy_count()
+	object_count_label.text = "Objects %d" % object_maker.get_object_count()
+	projectile_count_label.text = "Projectiles %d" % object_maker.get_projectile_count()
--- scenes/pause_menu/pause_menu.gd
+++ scenes/pause_menu/pause_menu.gd
@@ -1,24 +1,9 @@
 class_name PauseMenu extends Control
 
-@onready var enemy_count_label: Label = $VBoxContainer/EnemyCountLabel
-@onready var object_count_label: Label = $VBoxContainer/ObjectCountLabel
-@onready var projectile_count_label: Label = $VBoxContainer/ProjectileCountLabel
-
 
 func _notification(what: int) -> void:
 	match what:
 		Node.NOTIFICATION_PAUSED:
 			hide()
 		Node.NOTIFICATION_UNPAUSED:
-			update_debug_labels()
 			show()
-
-
-func update_debug_labels() -> void:
-	var object_maker: ObjectMaker = get_tree().root.get_node("Main/Level/ObjectMaker")
-	var enemy_count: int = object_maker.get_enemy_count()
-	var object_count: int = object_maker.get_object_count()
-	var projectile_count: int = object_maker.get_projectile_count()
-	enemy_count_label.text = "Enemy Count: " + str(enemy_count)
-	object_count_label.text = "Object Count: " + str(object_count)
-	projectile_count_label.text = "Projectile Count: " + str(projectile_count)
