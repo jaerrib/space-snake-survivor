4f8243942fd9a2b8d98adbd3f51942afb974b495
jaerrib <jaerrib@tutanota.com>
2025-09-02 05:56:59 -0400

    Stop music on player death

--- scenes/level/level.gd
+++ scenes/level/level.gd
@@ -1,7 +1,18 @@
 extends Node2D
 
+@onready var music: AudioStreamPlayer = $Music
+
+
+func _ready() -> void:
+	SignalManager.on_player_died.connect(on_player_died)
+
+
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("xp_spawn"):
 		var player: Snake = get_tree().get_first_node_in_group("player")
 		var spawn_pos: Vector2 = Vector2(player.global_position.x + 50, player.global_position.y + 50)
 		SignalManager.on_create_object.emit(spawn_pos, Constants.ObjectType["XP"])
+
+
+func on_player_died() -> void:
+	music.stop()
