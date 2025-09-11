7200a75565cdbcaf0d3b647d900d793e730ffaed
jaerrib <jaerrib@tutanota.com>
2025-09-11 05:45:03 -0400

    Modify logic for level handling

--- scenes/snake_attack_2/snake_attack_2.gd
+++ scenes/snake_attack_2/snake_attack_2.gd
@@ -2,6 +2,7 @@ class_name SnakeAttack2 extends Node2D
 
 const MULTIPLIER: float = .2
 const PROJECT_SPEED: float = 0.0
+const MAX_LEVEL: int = 5
 
 @export var damage: float = 5
 @export var delay_time: float = 3.0
@@ -39,12 +40,11 @@ func _on_cooldown_timer_timeout() -> void:
 	)
 
 
-
 func on_level_up() -> void:
-	if level_increases > 5:
+	if weapon_level >= MAX_LEVEL:
 		return
-	weapon_level += 1
-	if weapon_level % 10 == 0:
+	level_increases += 1
+	if level_increases % 10 == 0:
 		level_increases += 1
 		damage += (damage * MULTIPLIER)
 		delay_time *= (1 - MULTIPLIER)
