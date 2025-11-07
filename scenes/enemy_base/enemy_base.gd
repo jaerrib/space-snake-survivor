d7bdcabcecd92cfbc1c1cf57ca6f248463f2b065
jaerrib <jaerrib@tutanota.com>
2025-11-07 05:36:44 -0500

    Update sound call

--- scenes/enemy_base/enemy_base.gd
+++ scenes/enemy_base/enemy_base.gd
@@ -55,7 +55,7 @@ func _on_hit_box_area_entered(area: Area2D) -> void:
 		SignalManager.on_enemy_killed.emit()
 		queue_free()
 	else:
-		SoundManager.play_sound_at(SoundManager.SoundType.ENEMY_HIT, global_position)
+		SoundManager.play_sound_at(SoundDefs.SoundType.ENEMY_HIT, global_position)
 		knockback()
 
 
