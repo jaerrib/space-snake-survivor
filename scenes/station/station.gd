5d5fa51463e60f0c0011bfea093ba0188f8d0bd9
jaerrib <jaerrib@tutanota.com>
2025-11-17 05:44:59 -0500

    Reduce variable creation

--- scenes/station/station.gd
+++ scenes/station/station.gd
@@ -4,17 +4,20 @@ class_name Station extends CharacterBody2D
 @export var max_supply: float = 100.0
 @export var supply: float
 
+var player_ref: Snake = null
+var heal_amt: float = 0.0
+
 @onready var station_heal_supply: StationHealSupply = $StationHealSupply
 
 
 func _ready() -> void:
+	player_ref =  get_tree().get_first_node_in_group("player")
 	supply = max_supply
 	station_heal_supply.on_update_supply(supply)
 
 
 func get_heal_amt() -> float:
-	var player_ref: Snake =  get_tree().get_first_node_in_group("player")
-	var heal_amt: float = (player_ref.get_max_health() - player_ref.get_health()) #* heal_percentage
+	heal_amt = (player_ref.get_max_health() - player_ref.get_health()) #* heal_percentage
 	if supply - heal_amt > 0:
 		supply -= heal_amt
 	else:
