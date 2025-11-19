29b9495a761545dd2f8afad54b2a9d85bfaa6c04
jaerrib <jaerrib@tutanota.com>
2025-11-19 13:00:51 -0500

    Update speed and shoot position

--- scenes/alien_7/alien_7.gd
+++ scenes/alien_7/alien_7.gd
@@ -1,11 +1,11 @@
 class_name Alien7 extends EnemyBase
 
-var _projectile_speed: float = 30.0
+var _projectile_speed: float = 50.0
 var _attack_penetration: float = 5.0
 
 func _on_shoot_timer_timeout() -> void:
 	var direction = global_position.direction_to(player.global_position)
-	var shoot_position: Vector2 = Vector2(global_position.x, global_position.y - 8)
+	var shoot_position: Vector2 = Vector2(global_position.x, global_position.y)
 	velocity = direction * movement_speed
 	SignalManager.on_create_projectile.emit(
 		shoot_position,
