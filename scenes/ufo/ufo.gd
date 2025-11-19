38741a2352c32b8d393305f1237697b567cf97d2
jaerrib <jaerrib@tutanota.com>
2025-11-19 12:46:00 -0500

    Spawn alien_7 on UFO death

--- scenes/ufo/ufo.gd
+++ scenes/ufo/ufo.gd
@@ -8,6 +8,7 @@ func _on_hit_box_area_entered(area: Area2D) -> void:
 		SoundManager.play_sound_at(SoundDefs.SoundType.DIE03, global_position)
 		var spawn_pos: Vector2 = global_position
 		SignalManager.on_create_object.emit(spawn_pos, Constants.ObjectType["XP"], xp_val)
+		SignalManager.on_create_enemy.emit(spawn_pos, Constants.EnemyType.ALIEN_7)
 		SignalManager.on_enemy_killed.emit()
 		queue_free()
 	else:
