e84c48fff16abc1fcbe8429c90d6f4e52f0870c6
jaerrib <jaerrib@tutanota.com>
2025-11-27 06:02:54 -0500

    Reduce number of calls to get sizes and distances

--- scenes/enemy_spawner/enemy_spawner.gd
+++ scenes/enemy_spawner/enemy_spawner.gd
@@ -8,6 +8,8 @@ var difficulty_multiplier: float
 var sector_multiplier: int = 1
 var spawn_modifier: float
 var delay_modifier: float
+var view_size: Vector2
+var base_distance: float
 
 @onready var player: Snake =  get_tree().get_first_node_in_group("player")
 @onready var timer: Timer = $Timer
@@ -19,6 +21,8 @@ func _ready() -> void:
 	SignalManager.on_set_difficulty.connect(on_set_difficulty)
 	SignalManager.on_player_died.connect(on_player_died_or_level_complete)
 	on_set_modifiers()
+	view_size = get_viewport_rect().size
+	base_distance = max(view_size.x, view_size.y) / 4
 
 
 func on_player_died_or_level_complete() -> void:
@@ -64,8 +68,6 @@ func _on_timer_timeout() -> void:
 
 
 func get_random_position() -> Vector2:
-	var view_size: Vector2 = get_viewport_rect().size
-	var base_distance: float = max(view_size.x, view_size.y) / 4
 	var spawn_distance: float = randf_range(base_distance * 1.1, base_distance * 1.4)
 	var angle: float = randf_range(0, TAU)
 	var offset: Vector2 = Vector2.RIGHT.rotated(angle) * spawn_distance
