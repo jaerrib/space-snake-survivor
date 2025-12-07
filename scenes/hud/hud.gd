a6adbc2e145db38f24e188fcfbd6dd4ff6368772
jaerrib <jaerrib@tutanota.com>
2025-12-07 13:01:33 -0500

    Spawn boss further away

--- scenes/hud/hud.gd
+++ scenes/hud/hud.gd
@@ -82,8 +82,8 @@ func _on_level_timer_timeout() -> void:
 func get_random_position() -> Vector2:
 	var player: Snake =  get_tree().get_first_node_in_group("player")
 	var view_size: Vector2 = get_viewport_rect().size
-	var base_distance: float = max(view_size.x, view_size.y) / 4
-	var spawn_distance: float = randf_range(base_distance * 1.1, base_distance * 1.4)
+	var base_distance: float = max(view_size.x, view_size.y) / 2
+	var spawn_distance: float = randf_range(base_distance * 1.2, base_distance * 1.6)
 	var angle: float = randf_range(0, TAU)
 	var offset: Vector2 = Vector2.RIGHT.rotated(angle) * spawn_distance
 	return player.global_position + offset
