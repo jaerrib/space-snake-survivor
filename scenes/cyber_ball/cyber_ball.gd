12b5b095009d53bfc7193c97ad480b475a5aa5f1
jaerrib <jaerrib@tutanota.com>
2025-12-07 11:44:59 -0500

    Rename to clarify node function

--- scenes/cyber_ball/cyber_ball.gd
+++ scenes/cyber_ball/cyber_ball.gd
@@ -1,7 +1,7 @@
 extends BaseProjectile
 
 
-func _on_area_2d_area_entered(area: Area2D) -> void:
+func _on_hurt_box_area_entered(area: Area2D) -> void:
 	if area.get_parent().is_in_group("player"):
 		_penetration -= 1
 		check_max_penetation()
