66a6cd9d0f0f60c239ba7e8efea11e99318a3def
jaerrib <jaerrib@tutanota.com>
2025-12-07 12:09:37 -0500

    Remove debug print statement

--- scenes/base_projectile/base_projectile.gd
+++ scenes/base_projectile/base_projectile.gd
@@ -36,7 +36,6 @@ func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 
 
 func check_max_penetation() -> void:
-	print(_penetration)
 	if _penetration <= 0:
 		set_process(false)
 		queue_free()
