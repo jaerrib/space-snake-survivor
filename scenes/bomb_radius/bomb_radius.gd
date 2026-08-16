afe786e69e514a1c972682047494d71cb397c542
jaerrib <jaerrib@tutanota.com>
2026-08-16 07:07:50 -0400

    Fix unintended inheritance

--- scenes/bomb_radius/bomb_radius.gd
+++ scenes/bomb_radius/bomb_radius.gd
@@ -4,3 +4,7 @@ extends BaseProjectile
 
 func _on_animation_player_animation_finished(_anim_name: StringName) -> void:
 	queue_free()
+
+
+func play_bomb_sound() -> void:
+		SoundManager.play_sound_at(SoundDefs.SoundType.SNAKE_BOMB, global_position)
